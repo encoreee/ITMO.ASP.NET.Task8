e9fdb4d7ed205837703fa7db0f10b29303b629b9
auroraclub <alexraykov2000@gmail.com>
2021-04-28 23:19:01 +0300

    task 8

---
+++ GeekQuiz/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekQuiz.Models;
+
+namespace GeekQuiz.Controllers
+{
+
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var ctx = new TriviaDbContext())
+            {
+                ViewData["questionscount"] = ctx.TriviaQuestions.Count();
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GeekQuiz/Models/TriviaDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+
+namespace GeekQuiz.Models
+{
+    public class TriviaDbContext : DbContext
+    {
+        private static bool _created = false;
+
+        public TriviaDbContext() : base("name=DefaultConnection")
+        {
+            if (!_created)
+            {
+                _created = true;
+            }
+        }
+
+        //protected override void OnModelCreating(DbModelBuilder builder)
+        //{
+        //    //builder.Entity<TriviaOption>()
+        //    //    .HasKey(o => new { o.QuestionId, o.Id });
+
+        //    //builder.Entity<TriviaAnswer>()
+        //    //    .HasOptional(a => a.TriviaOption)
+        //    //    .WithMany()
+        //    //    .HasForeignKey(a => new { a.QuestionId, a.OptionId });
+
+        //    //builder.Entity<TriviaQuestion>()
+        //    //    .HasMany(q => q.Options)
+        //    //    .WithOptional(o => o.TriviaQuestion);
+        //}
+        
+        public DbSet<TriviaQuestion> TriviaQuestions { get; set; }
+
+        public DbSet<TriviaOption> TriviaOptions { get; set; }
+
+        public DbSet<TriviaAnswer> TriviaAnswers { get; set; }
+    }
+}
